e1edc37862d74b5c8047d9e90fee21c43adcddeb
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-03-02 08:59:33 +1100

    add toggleReminder feature

--- src/App.js
+++ src/App.js
@@ -34,11 +34,24 @@ function App() {
 		setTasks(tasks.filter(task => task.id !== id))
 	}
 
+	const toggleReminder = id => {
+		setTasks(tasks.map(task => 
+			task.id === id
+				? {...task, reminder: !task.reminder}
+				: task
+			)
+		);
+	}
+
   return (
     <div className="container">
       <Header title="Task Tracker" />
 			{tasks.length > 0 ? (
-				<Tasks tasks={tasks} onDelete={deleteTask} />
+				<Tasks
+					tasks={tasks}
+					onDelete={deleteTask}
+					onToggle={toggleReminder}
+				/>
 			) : (
 				<p>There are no tasks right now.</p>
 			)}
