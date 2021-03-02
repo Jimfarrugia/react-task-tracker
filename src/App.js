b67c11e6a0e57406795ff3398e1b536e76af64dd
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-03-02 20:07:05 +1100

    install json-server and implement 'fetchTasks' to get task data from (mock) db

--- src/App.js
+++ src/App.js
@@ -1,36 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 
 function App() {
 	const [showAddTask, setShowAddTask] = useState(false);
-	const [tasks, setTasks] = useState([
-		{
-			id: 1,
-			text: "Doctors Appt.",
-			day: "5th Feb at 2:30pm",
-			reminder: true
-		},
-		{
-			id: 2,
-			text: "Band Practice",
-			day: "7th Feb at 7:00pm",
-			reminder: true
-		},
-		{
-			id: 3,
-			text: "Food Shopping",
-			day: "5th Feb at 3:30pm",
-			reminder: false
-		},
-		{
-			id: 4,
-			text: "Lunch Meeting",
-			day: "8th Feb at 1:00pm",
-			reminder: true
-		},
-	])
+	const [tasks, setTasks] = useState([])
+
+	useEffect(() => {
+		const getTasks = async () => {
+			const tasksFromServer = await fetchTasks();
+			setTasks(tasksFromServer);
+		}
+		getTasks();
+	}, []);
+
+	const fetchTasks = async () => {
+		const res = await fetch('http://localhost:5000/tasks');
+		const data = await res.json();
+		return data;
+	}
 
 	const addTask = (task) => {
 		const id = Math.floor(Math.random() * 10000)+1;
